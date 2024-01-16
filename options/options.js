299f7ace501b4f2202700ee69c5968c20b2b2b61
Crowdbotics <engineering@crowdbotics.com>
2024-01-16 21:37:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "prdai-test-4",
+  url: "https://prdai-test-4.botics.co",
+  api: "https://prdai-test-4.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
